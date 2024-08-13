445c60bf26e9a5d2c40aa589fd46cdd2cabdd77f
sheung1819 <sheung1020@outlook.com>
2024-08-12 22:18:53 -0700

    Updated to add lock

--- RateLimiter.Repository/RateLimiterDataStore.cs
+++ RateLimiter.Repository/RateLimiterDataStore.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Caching.Memory;
-using RateLimiter.Interface;
+﻿using RateLimiter.Interface;
 using RateLimiter.Model;
 using System.Collections.Concurrent;
 
@@ -8,23 +7,30 @@ namespace RateLimiter.Repository
     public class RateLimiterDataStore : IRateLimiterRepository
     {
         private readonly ConcurrentDictionary<string, Request> _concurrentDictionary;
+        private readonly object _lock = new object();
+
         public RateLimiterDataStore()
         {
             _concurrentDictionary = new ConcurrentDictionary<string, Request>();
         }
+
         public Request Update(RequestDTO requestDTO)
         {
             if (_concurrentDictionary.ContainsKey(requestDTO.CallId))
             {
-                var currentRequest = _concurrentDictionary[requestDTO.CallId];
-                currentRequest.AccessTime.Add(requestDTO.CurrentTime);
-                _concurrentDictionary[requestDTO.CallId] = currentRequest;
-                return currentRequest;
+                lock (_lock)
+                {
+                    var currentRequest = _concurrentDictionary[requestDTO.CallId];
+                    currentRequest.AccessTime.Add(requestDTO.CurrentTime);
+                    _concurrentDictionary[requestDTO.CallId] = currentRequest;
+                    return currentRequest;
+                }
             }
-            var request = (Request)requestDTO;
+            var request = ConvertToRequest(requestDTO);
             _concurrentDictionary[requestDTO.CallId] = request;
             return request;
         }
+
         public Request Get(RequestDTO requestDTO)
         {
             if (_concurrentDictionary.ContainsKey(requestDTO.CallId))
@@ -32,9 +38,18 @@ namespace RateLimiter.Repository
                 return _concurrentDictionary[requestDTO.CallId];
             }
 
-            var request = (Request)requestDTO;
+            var request = ConvertToRequest(requestDTO);
             _concurrentDictionary[requestDTO.CallId] = request;
             return request;
+        }
+
+        private Request ConvertToRequest(RequestDTO requestDTO)
+        {            
+            return new Request
+            {
+                CallId = requestDTO.CallId,
+                AccessTime = new List<DateTime> { requestDTO.CurrentTime }
+            };
         }
     }
 }
