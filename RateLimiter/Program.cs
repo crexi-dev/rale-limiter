4f84eec7e54a04b18b9e33bbade2e9f7234fb6d6
Mike Griffin <mpgriffin5@gmail.com>
2024-12-10 14:35:21 -0800

    Add in-memory cache and classes for tracking client activity

--- RateLimiter/Program.cs
+++ RateLimiter/Program.cs
@@ -1,4 +1,5 @@
 using RateLimiter.Middleware;
+using RateLimiter.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSingleton<ClientBehaviorCache>();
 
 var app = builder.Build();
 
