5066e0e53a0258ea8b0d529665fc78fe37d83f91
Rey <reydelatorre@users.noreply.github.com>
2025-01-13 12:32:50 -0800

    3rd request within the same window, from the same client who already used its allowed request

--- RateLimiter.Tests/FixedWindowRuleTests.cs
+++ RateLimiter.Tests/FixedWindowRuleTests.cs
@@ -41,10 +41,32 @@ namespace RateLimiter.Tests
             // second request from different client but within same time window
             var rule = new FixedWindowRule(limit: 1, window: TimeSpan.FromMinutes(1), usageRepo);
             var firstRequestIsAllowed = rule.IsRequestAllowed("crexi-client123");
+            
+            // this verifies that each client is tracked independently
             var secondRequestIsAllowed = rule.IsRequestAllowed("crexi-client456");
 
             Assert.IsTrue(firstRequestIsAllowed, "first request should be allowed.");
-            Assert.IsTrue(firstRequestIsAllowed, "first request from different client should be allowed.");
+            Assert.IsTrue(secondRequestIsAllowed, "first request from different client should be allowed.");
+        }
+
+        [Test]
+        public void ThirdRequest_WithinSameWindowButFromSameClient_Should_BeBlocked()
+        {
+            var usageRepo = new InMemoryUsageRepository();
+
+            // second request from different client but within same time window
+            var rule = new FixedWindowRule(limit: 1, window: TimeSpan.FromMinutes(1), usageRepo);
+            var firstRequestIsAllowed = rule.IsRequestAllowed("crexi-client123");
+            var secondRequestIsAllowed = rule.IsRequestAllowed("crexi-client456");
+
+            // now ensure second client cannot break tiem window rule
+            var thirdRequestIsAllowed = rule.IsRequestAllowed("crexi-client456");
+
+            Assert.IsTrue(firstRequestIsAllowed, "first request should be allowed.");
+            Assert.IsTrue(secondRequestIsAllowed, "first request from second client should be allowed.");
+
+            // should be blocked
+            Assert.IsFalse(thirdRequestIsAllowed, "second request from second client should not be allowed.");
         }
     }
 }
