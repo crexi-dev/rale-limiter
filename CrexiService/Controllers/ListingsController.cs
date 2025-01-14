5a84aeeefb171c75bd0d01c0a2aa9478caf0d638
Rey <reydelatorre@users.noreply.github.com>
2025-01-13 17:09:31 -0800

    sample core webapi service to demonstrate rate limiter class lib

---
+++ CrexiService/Controllers/ListingsController.cs
@@ -0,0 +1,36 @@
+﻿using CrexiService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrexiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListingsController : ControllerBase
+    {
+        [HttpGet("listings")]
+        public IEnumerable<CommercialListing> GetListings()
+        {
+            return new List<CommercialListing>()
+            {
+                new CommercialListing
+                {
+                    ListingId = 101,
+                    Title = "Downtown Retail Space",
+                    Price = 2500000,
+                    City = "Los Angeles",
+                    State = "CA",
+                    BrokerName = "Alice Realty"
+                },
+                new CommercialListing
+                {
+                    ListingId = 102,
+                    Title = "Midtown Office Suite",
+                    Price = 1500000,
+                    City = "New York",
+                    State = "NY",
+                    BrokerName = "Spongebob & Associates"
+                }
+            };
+        }
+    }
+}
